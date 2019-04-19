c261e18e03b0a58de3d093ced8ad482efb7fe1b4
Ronan <rsf.sho@gmail.com>
2019-04-19 09:51:21 -0700

    updated code to improve error handling

--- index.js
+++ index.js
@@ -19,45 +19,71 @@ function getEventbriteAttendees(url) {
           'Authorization': 'Bearer ' + keys.eventbriteAPIKey,
         }
       }).then((resp) => resp.json()).then(data => {
-        //console.log(data)
-        const cleanedAttendees = data.attendees.map(i => {
-          const b = {
-            "resource_uri": i.resource_uri,
-            "id": i.id,
-            "changed": i.changed,
-            "created": i.created,
-            "quantity": i.quantity,
-            "variant_id": i.variant_id,
-            "profile": i.profile,
-            gender: i.gender,
-            age: i.age,
-            "birth_date": i.birth_date,
-            "email": i.email,
-            "name": i.name,
-            answers: i.answers,
-            "ticket_class_name": i.ticket_class_name,
-            "event_id": i.event_id,
-            "order_id": i.order_id,
-            "ticket_class_id": i.ticket_class_id
+        if (data.error) {
+          console.error(data.error, data.error_description, 'eventbrite fetch')
+        } else {
+          const cleanedAttendees = data.attendees.map(i => {
+            const b = {
+              "resource_uri": i.resource_uri,
+              "id": i.id,
+              "changed": i.changed,
+              "created": i.created,
+              "quantity": i.quantity,
+              "variant_id": i.variant_id,
+              "profile": i.profile,
+              gender: i.gender,
+              age: i.age,
+              "birth_date": i.birth_date,
+              "email": i.email,
+              "name": i.name,
+              answers: i.answers,
+              "ticket_class_name": i.ticket_class_name,
+              "event_id": i.event_id,
+              "order_id": i.order_id,
+              "ticket_class_id": i.ticket_class_id
+            }
+            eventbriteData.push(b)
+            return b
+          })
+
+          if (data.pagination.has_more_items) {
+            getEventbriteAttendees(`${keys.eventbriteURL}?continuation=${data.pagination.continuation}`)
           }
-          eventbriteData.push(b)
-          return b
-        })
-        if (data.pagination.has_more_items) {
-          getEventbriteAttendees(`${keys.eventbriteURL}?continuation=${data.pagination.continuation}`)
+          resolve(cleanedAttendees)
         }
-        resolve(cleanedAttendees)
+       // console.log(data)
+
       })
       .catch(function (e) {
-        console.log(e)
+        console.error(e)
       });
     }
   )
 }
 
-getEventbriteAttendees(keys.eventbriteURL).then((e) => {
-  console.log(eventbriteData)
-  fs.writeFile('./private/sample-api-return.json', JSON.stringify(eventbriteData), ()=> {
+const updateFirebaseWithNewEventbriteData =  async () => {
+  console.log('updating firebase with eventbrite...')
+  await getEventbriteAttendees(keys.eventbriteURL).then((e) => {
+    fs.writeFile('./private/sample-api-return.json', JSON.stringify(eventbriteData), () => {
+    })
+    const peopleRef = db.collection('people');
+    eventbriteData.forEach(i => {
+      //console.log(JSON.stringify(i))
+      peopleRef.doc(i.id).get().then((docSnapshot) => {
+        if (docSnapshot.exists) {
+        } else {
+          let e = JSON.stringify(i)
+          peopleRef.doc(i.id).set(JSON.parse(e));
+          console.log(`added ${i.id}`)
+        }
+      });
 
+    })
   })
+  console.log('done updating')
+  return true
+}
+
+updateFirebaseWithNewEventbriteData().then(e => {
+console.log('next')
 })
