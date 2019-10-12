b103c5d79a931a5759c20b584e9c55b7fb2cbc49
Ronan <rsf.sho@gmail.com>
2019-10-11 23:33:24 -0700

    simply the notification

--- index.js
+++ index.js
@@ -656,10 +656,10 @@ app.post('/api/v3/pushnotification', (req, res) => {
           if (sent.indexOf(pushToken) === -1) {
             const message = {
               to: pushToken,
-              sound: 'default',
+              //  sound: 'default',
               title: body.title,
               body: body.message,
-              icon: 'https://i.imgur.com/fnB0g5p.png'
+              // icon: 'https://i.imgur.com/fnB0g5p.png'
             }
             // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
             messages.push(message)
